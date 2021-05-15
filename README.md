5703a19f5bf675a50e248c38bce93cc8a5eafc68
ElMayo <mic.l.meier@gmail.com>
2021-05-15 17:09:06 +0200

    feat: Add tailwind

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Basic [next.js](https://nextjs.org/) starter project with [tailwind.css](https://tailwindcss.com/) for styling
+
 Create a new project using this starter:
 
 ```bash
@@ -5,7 +7,7 @@ yarn create next-app <project-name> -e https://github.com/mic-meier/elmayos-next
 cd <project-name> && yarn install
 ```
 
-Run the starter>
+Run the starter:
 
 ```bash
 yarn dev
--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,15 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>
-          Welcome to <a href="https://github.com/mic-meier">El Mayo&apos;s</a>{' '}
-          next.js starter
-        </h1>
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-7xl">
+            Welcome to{' '}
+            <a href="https://github.com/mic-meier" className="mx-auto">
+              El Mayo&apos;s
+            </a>{' '}
+            next.js starter
+          </h1>
+        </div>
       </main>
     </div>
   )
